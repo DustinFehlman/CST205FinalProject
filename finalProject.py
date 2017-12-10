b924ddf02365f3f687db40bf69851609b94d3e64
Dustin Fehlman <dfehlman@csumb.edu>
2017-12-10 14:52:39 -0800

    Added scoreboard class

--- finalProject.py
+++ finalProject.py
@@ -1,1 +1,28 @@
-#Team Educode Final Porject
+#Team Educode Final Project
+
+class Scoreboard:
+  score = 0
+  playerName = None
+  
+  def __init__(self, playerName):
+    self.playerName = playerName
+    
+  def getScore():
+    return score
+  
+  def getPlayerName():
+    return playerName
+    
+  def setScore(score):
+    self.score = score
+  
+  def addScore(add):
+    self.score += add
+    
+  def subtractScore(sub):
+    self.score -= sub
+    
+def main():
+  scoreboard = Scoreboard("test")
+  
+  
